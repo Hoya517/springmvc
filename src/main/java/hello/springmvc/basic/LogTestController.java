9c3a3203878e9dc719156b7656f1327688f54623
Hoya517 <61738279+Hoya517@users.noreply.github.com>
2021-07-04 18:37:45 +0900

    로깅 간단히 알아보기

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,30 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+//    private final Logger log = LoggerFactory.getLogger(getClass());  // LogTestController.class
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+
+        // 로그 어떤 상태의 레벨
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);  // 디버그할때 (개발서버에서)
+        log.info(" info log={}", name);   // 중요한 비즈니스 정보 (운영서버)
+        log.warn(" warn log={}", name);   // 경고
+        log.error("error log={}", name);  // 에러 (알람, 파일 등등 활용해서 확인)
+
+        return "ok";
+    }
+}
